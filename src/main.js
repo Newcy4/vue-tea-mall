1746c0ed4025af6705e2eae7c20cc03a95c203c1
阿四 <779213589@qq.com>
2022-07-16 23:00:15 +0800

    实现Tabbar

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+// 引入公共样式文件
+import '@/assets/styles/global.less'
+// 引入淘宝无限适配文件
+import '@/assets/js/flexible.js'
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,27 @@
+// 配置@路径所需要的包
+// let path = require('path')
+
+module.exports = {
+  //代理
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        pathRewrite: {
+          '/api': '/api'
+        }
+      }
+    }
+  }
+  /* 其实Vue已经给我们配置好了，这里了解即可
+  configurewebpack: (config) => {
+    config.resolve = {
+      extensions: ['.js', '.json', '.vue'],
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    }
+  }
+  */
+}
